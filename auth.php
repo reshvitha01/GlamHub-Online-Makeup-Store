ebb2db23399b123dbb5e7ece350d9405870ae542
perarasi1206 <perarasi1206@gmail.com>
2026-06-09 17:49:37 +0800

    Add files via upload

--- auth.php
+++ auth.php
@@ -12,4 +12,26 @@ function currentUserName()
 {
     return $_SESSION['user_name'] ?? 'Guest';
 }
+
+function currentUserRole()
+{
+    return $_SESSION['user_role'] ?? 'guest';
+}
+
+function hasRole($roles)
+{
+    if (!is_array($roles)) {
+        $roles = [$roles];
+    }
+
+    return isLoggedIn() && in_array(currentUserRole(), $roles, true);
+}
+
+function requireRole($roles)
+{
+    if (!hasRole($roles)) {
+        header('Location: login.php?restricted=1');
+        exit;
+    }
+}
 ?>
--- header.php
+++ header.php
@@ -23,11 +23,34 @@ $pageTitle = $pageTitle ?? 'GlamHub Cosmetics';
         </button>
         <div class="collapse navbar-collapse" id="mainNav">
             <ul class="navbar-nav ms-auto align-items-lg-center gap-lg-2">
-                   <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                   <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
-                   <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                <li class="nav-item"><a class="nav-link" href="shade_finder.php">Shade Finder</a></li>
+                <li class="nav-item"><a class="nav-link" href="products.php">Products</a></li>
 
                 <?php if (isLoggedIn()): ?>
+                    <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
+                    <li class="nav-item"><a class="nav-link" href="wishlist.php">Wishlist</a></li>
+                    <li class="nav-item"><a class="nav-link" href="recommendation_history.php">History</a></li>
+                    <li class="nav-item"><a class="nav-link" href="orders.php">Orders</a></li>
+
+                    <?php if (hasRole(['staff', 'admin'])): ?>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Management
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><a class="dropdown-item" href="staff.php"><i class="bi bi-clipboard-check me-2"></i>Staff Orders</a></li>
+                                <?php if (hasRole('admin')): ?>
+                                    <li><a class="dropdown-item" href="admin.php"><i class="bi bi-speedometer2 me-2"></i>Admin Dashboard</a></li>
+                                    <li><a class="dropdown-item" href="admin_products.php"><i class="bi bi-box-seam me-2"></i>Manage Products</a></li>
+                                    <li><a class="dropdown-item" href="admin_orders.php"><i class="bi bi-receipt me-2"></i>All Orders</a></li>
+                                    <li><a class="dropdown-item" href="manage_accounts.php"><i class="bi bi-person-gear me-2"></i>Manage Accounts</a></li>
+                                <?php endif; ?>
+                            </ul>
+                        </li>
+                    <?php endif; ?>
+
                     <li class="nav-item">
                         <span class="nav-link text-muted">Hi, <?php echo htmlspecialchars(currentUserName()); ?></span>
                     </li>
